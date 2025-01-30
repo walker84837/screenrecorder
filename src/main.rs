89fb9c81753f21310a72d3f517008d8b16a9ae0e
EmirhanTr3 <emirhaneren20072010@gmail.com>
2025-01-30 23:34:07 +0300

    yes

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::{sync::{atomic::{AtomicI32, Ordering}, Arc}, thread, time::{Duration, Instant}};
+
+use crabgrab::prelude::{CapturableContent, CapturableContentFilter, CaptureConfig, CaptureStream};
+
+#[tokio::main]
+async fn main() {
+    start_recorder().await;
+    loop { thread::yield_now(); }
+}
+
+async fn start_recorder() {
+    let fps = Arc::new(AtomicI32::new(0));
+    
+    let fps_loop = Arc::clone(&fps);
+    thread::spawn(move || {
+        loop {
+            println!("fps: {}", fps_loop.load(Ordering::Relaxed));
+            fps_loop.store(0, Ordering::Relaxed);
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    let token = match CaptureStream::test_access(false) {
+        Some(token) => token,
+        None => CaptureStream::request_access(false).await.expect("Expected capture access")
+    };
+    let filter = CapturableContentFilter::EVERYTHING;
+    let content = CapturableContent::new(filter).await.unwrap();
+    let config = CaptureConfig::with_display(content.displays().next().unwrap(), CaptureStream::supported_pixel_formats()[0]);
+
+    let fps_recorder = Arc::clone(&fps);
+    loop {
+        let result = crabgrab::feature::screenshot::take_screenshot(token, config.clone()).await;
+        let start = Instant::now();
+        println!("received frame in {}ms", start.elapsed().as_millis());
+        fps_recorder.fetch_add(1, Ordering::Relaxed);
+    }
+}
